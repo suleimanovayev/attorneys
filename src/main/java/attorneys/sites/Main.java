5499f0b3754bed6730ca16d81630513b09e20deb
lenovo <suleimanovayev@gmail.com>
2020-07-09 01:22:19 +0300

    fix main

--- src/main/java/attorneys/sites/Main.java
+++ src/main/java/attorneys/sites/Main.java
@@ -1,11 +1,7 @@
 package attorneys.sites;
 
-import attorneys.sites.gflegal.constant.GflegalConstants;
-import attorneys.sites.hellsell.constant.HellSellConstants;
 import attorneys.sites.thread.SitesRunner;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class Main {
